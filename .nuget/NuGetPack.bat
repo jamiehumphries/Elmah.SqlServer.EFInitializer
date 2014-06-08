74369cfed14bd9487cdebe54ea7515e9f19fc4f7
Jamie Humphries <jamie.humphries@softwire.com>
2014-06-08 17:11:21 +0100

    Move content folder within project.

--- .nuget/NuGetPack.bat
+++ .nuget/NuGetPack.bat
@@ -1,5 +1,6 @@
 @echo off
 set /p version="Version: "
 xcopy ..\Elmah.SqlServer.EFInitializer\bin\Release\Elmah.SqlServer.EFInitializer.dll lib\4.5\ /Y
+xcopy ..\Elmah.SqlServer.EFInitializer\content content\ /S /Y
 nuget pack Elmah.SqlServer.EFInitializer.nuspec -Version %version%
 pause
