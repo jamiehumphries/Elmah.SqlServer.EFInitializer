21f5280b02313f771c9e79772266dba105810dda
Justin Yoo <justin.yoo@aliencube.com>
2015-05-08 14:11:59 +1000

    Add Elmah.SqlServer.EFInitializer.Tests project
    - Added a method to confirm if a record is added or not.

---
+++ Elmah.SqlServer.EFInitializer.Tests/ElmahContextTest.cs
@@ -0,0 +1,46 @@
+﻿namespace Elmah.SqlServer.EFInitializer.Tests
+{
+    using System;
+    using System.IO;
+
+    using FluentAssertions;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    class ElmahContextTest
+    {
+        private ElmahContext _context;
+
+        [SetUp]
+        public void Init()
+        {
+            this._context = new ElmahContext("DefaultConnection");
+            var connectionString = this._context.Database.Connection.ConnectionString.Replace("|DataDirectory|", AppDomain.CurrentDomain.BaseDirectory);
+            this._context.Database.Connection.ConnectionString = connectionString;
+            AppDomain.CurrentDomain.SetData("DataDirectory", System.IO.Directory.GetCurrentDirectory());
+        }
+
+        [TearDown]
+        public void Cleanup()
+        {
+            if (this._context != null)
+            {
+                this._context.Dispose();
+            }
+        }
+
+        [Test]
+        [TestCase("TestApplication", "localhost", "OneType", "ReliableSource", "Test message", "TestUser", "<error>XmlError</error>", 500)]
+        public async void GivenParameters_Should_AddRecord(string application, string host, string type, string source, string message, string user, string allXml, int statusCode)
+        {
+            var errorId = Guid.NewGuid();
+            var utcNow = DateTime.UtcNow;
+            var result = await this._context.LogErrorAsync(errorId, application, host, type, source, message, user, allXml, statusCode, utcNow);
+
+            //result.Should().Be(1);
+
+            var error = await this._context.GetErrorXmlAsync(application, errorId);
+        }
+    }
+}
