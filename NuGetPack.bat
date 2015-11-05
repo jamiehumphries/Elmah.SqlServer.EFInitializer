bd3fa8874397d314f2f386305b669c970cf08418
lvv83 <vlozhkin83@gmail.com>
2015-11-06 01:35:17 +0300

    Multiple target frameworks for the project. Add NET 4.0 framework support to the nuget package. Some fixes at NuGetPack.bat

--- NuGetPack.bat
+++ NuGetPack.bat
@@ -1,9 +1,11 @@
 @echo off
 set /p version="Version: "
-msbuild Elmah.SqlServer.EFInitializer.sln /P:Configuration=Release
-rmdir /S /Q content
-rmdir /S /Q lib
+msbuild Elmah.SqlServer.EFInitializer.sln /P:Configuration=Release-NET40
+msbuild Elmah.SqlServer.EFInitializer.sln /P:Configuration=Release-NET45
+rmdir /S /Q nuget-pack\content
+rmdir /S /Q nuget-pack\lib
 xcopy Elmah.SqlServer.EFInitializer\content nuget-pack\content\ /S /Y
-xcopy Elmah.SqlServer.EFInitializer\bin\Release\Elmah.SqlServer.EFInitializer.dll nuget-pack\lib\4.5\ /Y
+xcopy Elmah.SqlServer.EFInitializer\bin\Release-NET40\Elmah.SqlServer.EFInitializer.dll nuget-pack\lib\net40\ /Y
+xcopy Elmah.SqlServer.EFInitializer\bin\Release-NET45\Elmah.SqlServer.EFInitializer.dll nuget-pack\lib\net45\ /Y
 .nuget\nuget pack nuget-pack\Elmah.SqlServer.EFInitializer.nuspec -Version %version%
 pause
